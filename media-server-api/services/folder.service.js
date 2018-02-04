6e9a403ae8d6c13451bb6bc11c3e7092e585da90
Noman Hasan <nomanbinhussein@gmail.com>
2018-02-05 00:43:04 +0600

    Folder API updated

--- media-server-api/services/folder.service.js
+++ media-server-api/services/folder.service.js
@@ -1,19 +1,31 @@
-var Folder = require('../models/folder');
+const Folder = require('../models/folder');
+const R = require('ramda');
 
 _this = this;
 
+const fileFields = 'name title artist album year duration path';
+const folderFields = 'name type'
+
 exports.getFolders  = async (page = 1, limit = 0) => {
+
+    const root = R.last(process.env.DEFAULT_DIRECTORY.split("/"));
+
+    
 
     const options = {
         page,
         limit
     }
 
-    const folders = await Folder.findOne({name: "Music"});
+    const folders = await Folder.findOne({name: root})
+    .populate('files', fileFields)
+    .populate('folders', folderFields);;
     return folders;
 }
 
 exports.getFolderById = async (id) => {
-    const folder = await Folder.findOne({_id: id }).populate('files');
+    const folder = await Folder.findOne({_id: id })
+    .populate('files', fileFields)
+    .populate('folders', folderFields);
     return folder;
 }
